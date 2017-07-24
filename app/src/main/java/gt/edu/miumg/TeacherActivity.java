c31e0e4dc6f89c9364ecb9412d75833219b3d263
Allan Garrido <allan-garrido@users.noreply.github.com>
2017-07-24 13:52:01 -0600

    ListView con dos Líneas

--- app/src/main/java/gt/edu/miumg/TeacherActivity.java
+++ app/src/main/java/gt/edu/miumg/TeacherActivity.java
@@ -10,11 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TeacherActivity extends AppCompatActivity {
     EditText et1, et2;
@@ -40,6 +43,8 @@ public class TeacherActivity extends AppCompatActivity {
         ArrayList<String> especialidad = new ArrayList<>();
         ArrayAdapter<String> ladapter1, ladapter2;
         ListAdapter la;
+        List<Map<String,String>> valores = new ArrayList<>();
+
 
         bdProfesor.insert("profesor",null,tupla);
 
@@ -57,10 +62,18 @@ public class TeacherActivity extends AppCompatActivity {
             do {
                 nombre.add(bdCursor.getString(bdCursor.getColumnIndex("nombre")));
                 especialidad.add(bdCursor.getString(bdCursor.getColumnIndex("especialidad")));
+
+                Map<String,String> lineas = new HashMap<>(2);
+
+                lineas.put("First Line",bdCursor.getString(bdCursor.getColumnIndex("nombre")));
+                lineas.put("Second Line",bdCursor.getString(bdCursor.getColumnIndex("especialidad")));
+                valores.add(lineas);
             } while (bdCursor.moveToNext());
             ladapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombre);
 
-            lv1.setAdapter(ladapter1);
+            SimpleAdapter sa = new SimpleAdapter(this, valores, android.R.layout.simple_list_item_2, new String[] {"First Line","Second Line"}, new int[] {android.R.id.text1, android.R.id.text2});
+
+            lv1.setAdapter(sa);
         }
 
         bdProfesor.close();
